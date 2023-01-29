f81f3bed1f7552e8085e0267f6dd3fea3dbc2a53
Isengrine <isengrine@gmail.com>
2023-01-29 04:50:46 -0600

    Add canvas and pixel function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function createCanvas(size) {
+    console.log("canvas created");
+    for (let i = 0; i < size; i++) {
+        const pixel = document.createElement("div");
+        pixel.setAttribute("class", "pixel");
+        canvas.appendChild(pixel);
+    }
+}
+
+let canvas = document.getElementById("canvas");
 let value = document.getElementById("value");
 let slider = document.getElementById("slider");
+let size = slider.value
 value.textContent = slider.value;
+
+window.onload = createCanvas(size);
 
 slider.oninput = () => {
     value.textContent = slider.value;
