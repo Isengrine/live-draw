78e74dcbe4e36d4d5b4d8b773d9de6bb0bd4180d
Isengrine <isengrine@gmail.com>
2023-01-29 22:47:12 -0600

    Add clear button

--- script.js
+++ script.js
@@ -22,10 +22,16 @@ function removePixels() {
 let canvas = document.getElementById("canvas");
 let value = document.getElementById("value");
 let slider = document.getElementById("slider");
-let size = slider.value
+let clear = document.getElementById("clearbtn");
+let size = slider.value;
 value.textContent = slider.value;
 
 window.onload = createCanvas(size);
+
+clear.addEventListener("click", () => {
+    removePixels();
+    createCanvas(size);
+})
 
 slider.oninput = () => {
     value.textContent = slider.value;
